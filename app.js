58ffceec80689a8f6032015371c033a5db958375
JessieZ <jessiejxc@gmail.com>
2018-12-02 15:20:11 -0500

    Init commit

---
+++ app.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// routes
+const personRoutes = require('./api/routes/persons');
+
+mongoose.connect('mongodb+srv://' + process.env.DB_AUTH + '@cluster0-knne7.mongodb.net/AuthorTitle?retryWrites=true', {
+    useNewUrlParser: true
+});
+
+mongoose.Promise = Promise;
+
+mongoose.connection.once('open', () => {
+    console.log('Connection with the db has been made');
+}).on('error', (error) => {
+    console.log('Connection error ', error);
+});
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/apijc1/persons', personRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
+// app.use((req, res, next) => {
+//     res.status(200).json({
+//         message: 'foo working lol'
+//     });
+// });
+
+module.exports = app;
